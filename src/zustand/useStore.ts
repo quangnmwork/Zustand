07cfa125b2dfaa2800a96b060f9ec6b25a364516
Quang Nguyen <quangnm.work@gmail.com>
2022-03-17 19:23:41 +0700

    add middleware without immer

--- src/zustand/useStore.ts
+++ src/zustand/useStore.ts
@@ -1,5 +1,7 @@
-import create, { SetState, GetState } from "zustand";
+import create, { SetState, GetState, StateCreator, StoreApi } from "zustand";
+
 import { persist } from "zustand/middleware";
+
 interface CountStore {
   count: number;
   increment: () => void;
@@ -14,6 +16,20 @@ interface DogList {
   dogList: any;
   fetchDogList: () => void;
 }
+type Middleware<S extends Theme> = (
+  config: StateCreator<S>
+) => (set: SetState<S>, get: GetState<S>, api: StoreApi<S>) => S;
+const log: Middleware<Theme> = (config) => (set, get, api) =>
+  config(
+    (args: any) => {
+      console.log("Applying", args);
+      set(args);
+      console.log("new state", get());
+    },
+    get,
+    api
+  );
+
 export const useStore = create<CountStore>(
   persist(
     (set: SetState<CountStore>, get: GetState<CountStore>) => ({
@@ -38,13 +54,13 @@ export const useStore = create<CountStore>(
   )
 );
 export const useTheme = create<Theme>(
-  (set: SetState<Theme>, get: GetState<Theme>) => ({
+  log((set: SetState<Theme>, get: GetState<Theme>) => ({
     darkMode: false,
     toggleDarkMode: (): void => {
       const { darkMode } = get();
       set({ darkMode: !darkMode });
     },
-  })
+  }))
 );
 export const useDogList = create<DogList>(
   (set: SetState<DogList>, get: GetState<DogList>) => ({
